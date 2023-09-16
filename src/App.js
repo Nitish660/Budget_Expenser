3b5cde96eb19b40b4467d6d92d8fc677d009c384
Nitish660 <20051742@kiit.ac.in>
2023-09-16 22:44:36 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import styled from 'styled-components';
 
-function App() {
+const Container = styled.div`
+display: flex;
+flex-direction: column;
+ `;
+
+
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <Container>App
+      <header>
+    Expense tracker
       </header>
-    </div>
+    </Container>
   );
-}
+};
 
 export default App;
