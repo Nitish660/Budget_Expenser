4738e41fa173d19f1184d2b38f79019cbde97c99
Nitish660 <20051742@kiit.ac.in>
2023-09-17 23:16:22 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -2,18 +2,23 @@ import React from 'react';
 import styled from 'styled-components';
 
 const Container = styled.div`
-display: flex;
-flex-direction: column;
- `;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin: 30x 0 10px;
+  font-family: Montserrat;
+`;
 
-
+const Header = styled.span`
+  color: black;
+  font-size: 25px;
+  font-weight: bold;
+`;
 
 const App = () => {
   return (
-    <Container>App
-      <header>
-    Expense tracker
-      </header>
+    <Container>
+      <Header>Expense tracker</Header>
     </Container>
   );
 };
