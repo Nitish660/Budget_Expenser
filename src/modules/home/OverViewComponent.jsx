d6743a3b11b9883bbcf437e82fee53b2bd475670
Nitish660 <20051742@kiit.ac.in>
2023-09-19 21:01:28 +0530

    commit

--- src/modules/home/OverViewComponent.jsx
+++ src/modules/home/OverViewComponent.jsx
@@ -1,27 +1,50 @@
-import React, { useState } from "react";
+import React, {  useState } from "react";
 import styled from "styled-components";
 
 const Container = styled.div`
-  background-color: white;
-  color: #0d1d2c;
   display: flex;
   flex-direction: column;
-  padding: 10px 22px;
-  font-size: 18px;
-  width: 360px;
+  margin: 10px;
   align-items: center;
-  justify-content: space-between;
+  font-size: 16px;
+  width: 100%;
 `;
-
+const ExpenseContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  gap: 12px;
+  margin: 20px;
+`;
+const ExpenseBox = styled.div`
+  border-radius: 4px;
+  border: 1px solid #e6e8e9;
+  padding: 15px 20px;
+  font-size: 14px;
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+  width: 135px;
+  & span {
+    color: ${(props) => (props.isIncome ? "green" : "red")};
+    font-weight: bold;
+    font-size: 20px;
+  }
+`;
 const BalanceBox = styled.div`
+  font-size: 18px;
   display: flex;
   flex-direction: row;
   justify-content: space-between;
   align-items: center;
   width: 100%;
   font-weight: bold;
+  & span {
+    color: #0d1d2c;
+    opacity: 80%;
+    font-weight: bold;
+    font-size: 20px;
+  }
 `;
-
 const AddTransaction = styled.div`
   font-size: 15px;
   background: #0d1d2c;
@@ -32,14 +55,10 @@ const AddTransaction = styled.div`
   flex-direction: row;
   border-radius: 4px;
   font-weight: bold;
-  text-align: center; /* Apply text-align to center text within the div */
-  justify-content: center; /* Center horizontally */
-  align-items: center; /* Center vertically */
 `;
-
-
 const AddTransactionContainer = styled.div`
   font-size: 15px;
+  display: ${(props) => (props.isAddTxnVisible ? "flex" : "none")};
   color: #0d1d2c;
   flex-direction: column;
   border-radius: 4px;
@@ -52,98 +71,104 @@ const AddTransactionContainer = styled.div`
   & input {
     width: 90%;
     outline: none;
+    padding: 10px 12px;
     border-radius: 4px;
     border: 1px solid #e6e8e9;
-    margin: 5px;
-    padding: 5px;
   }
 `;
-
 const RadioBox = styled.div`
   display: flex;
   flex-direction: row;
   width: 100%;
   align-items: center;
   margin: 10px 0;
-  & input{
+  & input {
     width: unset;
     margin: 0 10px;
   }
 `;
+const AddTransactionView = (props) => {
+  const [amount, setAmount] = useState();
+  const [desc, setDesc] = useState();
+  const [type, setType] = useState("EXPENSE");
 
-const AddTransationView = (props) => {
-    const[amount,setAmount] = useState();
-    const[desc,setDesc] = useState();
-    const[type,setType] = useState("EXPENSE")
-
-    const addTransaction =(props) =>{
-      props.addTransaction({
-        id:Date.now(),
-        amount: Number(amount),
-        desc,type
-      })
-      console.log({amount,desc,type});
-      props.toggleAddTxn();
-    }
-
-  return <AddTransactionContainer>
-    <input
-    placeholder="Amount"
-    type="number"
-    value={amount}
-    onChange={(e) => setAmount(e.target.value)}
-    />
-    <input
-     placeholder="Description"
-     type="text"
-     value={desc}
-     onChange={(e) => setDesc(e.target.value)}
-    />
-
-    <RadioBox>
+  return (
+    <AddTransactionContainer isAddTxnVisible={props.isAddTxnVisible}>
       <input
-      type="radio"
-      id="expense"
-      name="type"
-      value="EXPENSE"
-      checked={type === "EXPENSE"}
-      onChange={(e) => setType(e.target.value)}
+        placeholder="Amount"
+        type="number"
+        value={amount}
+        onChange={(e) => setAmount(e.target.value)}
       />
-
-      <label htmlFor="expense">Expense</label>
-       <input
-     type="radio"
-     id="income"
-     name="type"
-     value="INCOME"
-     checked={type === "INCOME"}
-     onChange={(e) => setType(e.target.value)}
+      <input
+        placeholder="Description"
+        value={desc}
+        onChange={(e) => setDesc(e.target.value)}
       />
-       <label htmlFor="income">Income</label>
-    </RadioBox>
-    <AddTransaction onClick={addTransaction}
-    >Add Transaction</AddTransaction>
+      <RadioBox>
+        <input
+          type="radio"
+          id="expense"
+          name="type"
+          value="EXPENSE"
+          checked={type === "EXPENSE"}
+          onChange={(e) => setType(e.target.value)}
+        />
+        <label htmlFor="expense">Expense</label>
+        <input
+          type="radio"
+          id="income"
+          name="type"
+          value="INCOME"
+          checked={type === "INCOME"}
+          onChange={(e) => setType(e.target.value)}
+        />
+        <label htmlFor="Expense">Income</label>
+      </RadioBox>
 
-    
-  </AddTransactionContainer>;
+      <AddTransaction
+        onClick={() =>
+          props.addTransaction({
+            id: Date.now(),
+            amount: Number(amount),
+            desc,
+            type,
+          })
+        }
+      >
+        Add Transaction
+      </AddTransaction>
+    </AddTransactionContainer>
+  );
 };
-
 const OverViewComponent = (props) => {
-  const [isAddTxnVisible, toggleAddTxn] = useState(false);
-
+  const [isAddTxnVisible, toggleAddTXn] = useState(false);
   return (
     <Container>
       <BalanceBox>
-        Balance: $5000
-        <AddTransaction onClick={() => toggleAddTxn(!isAddTxnVisible)}
-        >{isAddTxnVisible ? "Cancel" : "Add"}</AddTransaction>
+        Balance: ${props.income - props.expense}
+        <AddTransaction onClick={() => toggleAddTXn((isVisible) => !isVisible)}>
+          {isAddTxnVisible ? "CANCEL" : "ADD"}
+        </AddTransaction>
       </BalanceBox>
-      {isAddTxnVisible && (<AddTransationView toggleAddTxn={toggleAddTxn} 
-      addTransaction={props.addTransaction}
-      />
-    )}
+      {isAddTxnVisible && (
+        <AddTransactionView
+          isAddTxnVisible={isAddTxnVisible}
+          addTransaction={(payload) => {
+            props.addTransaction(payload);
+            toggleAddTXn((isVisible) => !isVisible);
+          }}
+        />
+      )}
+      <ExpenseContainer>
+        <ExpenseBox>
+          Expense<span>${props.expense}</span>
+        </ExpenseBox>
+        <ExpenseBox isIncome={true}>
+          Income<span>${props.income}</span>
+        </ExpenseBox>
+      </ExpenseContainer>
     </Container>
-  )
+  );
 };
-
 export default OverViewComponent;
--- src/modules/home/index.jsx
+++ src/modules/home/index.jsx
@@ -1,7 +1,7 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import styled from "styled-components";
 import OverViewComponent from "./OverViewComponent";
-import TransactionsComponent from "./TransactionsComponent"
+import TransactionsComponent from "./TransactionsComponent";
 
 const Container = styled.div`
   background-color: white;
@@ -15,25 +15,42 @@ const Container = styled.div`
   justify-content: space-between;
 `;
 
-
 const HomeComponent = (props) => {
-    const[transactions,updateTransaction] = useState([]);
+    const [transactions, updateTransaction] = useState([]);
+    const [expense, updateExpense] = useState(0);
+    const [income, updateIncome] = useState(0);
+
+    const calculateBalance = () => {
+        let exp = 0;
+        let inc = 0;
+        transactions.map((payload) =>
+            payload.type === "EXPENSE"
+                ? (exp = exp + payload.amount)
+                : (inc = inc + payload.amount),
+        );
+        updateExpense(exp);
+        updateIncome(inc);
+    };
+    useEffect(() => calculateBalance(), [transactions]);
 
     const addTransaction = (payload) => {
         const transactionArray = [...transactions];
         transactionArray.push(payload);
         updateTransaction(transactionArray);
     };
-
     return (
         <Container>
-            
-            <OverViewComponent addTransaction={addTransaction}/>
-            <TransactionsComponent transactions={transactions}/>
+            <OverViewComponent
+                expense={expense}
+                income={income}
+                addTransaction={addTransaction}
+            />
+            {transactions?.length ? (
+                <TransactionsComponent transactions={transactions} />
+            ) : (
+                ""
+            )}
         </Container>
-
     );
-
 };
-
 export default HomeComponent;
